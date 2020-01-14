05156bd7aaf3d1ca506dc3e7aea5fc8a7b4498d7
Zakhar Hurov <gurov.zahar@gmail.com>
2020-01-14 22:48:39 +0100

    Vector of Students was added

--- students.cpp
+++ students.cpp
@@ -37,10 +37,31 @@ int main()
     // Additional extension - it shows how much time our 
     auto start = chrono::high_resolution_clock::now();
 
-          
+    vector <Person> Students 
+        Person("Zakhar",192,22),
+        Person("Misha", 129,21),
+        Person("Sergey", 200, 16),
+        Person("Anastasia", 200,21),
+        Person("Aleksandr",129,33),
+        Person("Ignat",123,34),
+        Person("Dmitriy",139,32),
+        Person("Katya",194,19),
+        Person("Polina",199,18),
+        Person("Marina", 181,21),
+        Person("Artur", 189,19),
+        Person("Masha", 85,21)
+    };
 
+    auto SuitableStudents = remove_if(Students.begin(), Students.end(), [](const Person& person)
 
+        {
+            return person.age < 30 && person.age > 18;
+        }
+    );
+      
 
+   
+    
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<float>duration = end - start;
     cout << "Duration is = " << duration.count() << " sec" << endl;
